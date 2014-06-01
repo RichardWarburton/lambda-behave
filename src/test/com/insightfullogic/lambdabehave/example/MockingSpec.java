c3cf1a6b4f1932b474dc5952018b2d5407168e27
Richard Warburton <richard.warburton@gmail.com>
2014-05-31 22:51:56 -0700

    add some syntactic mocking experiments

---
+++ src/test/com/insightfullogic/lambdabehave/example/MockingSpec.java
@@ -0,0 +1,55 @@
+package com.insightfullogic.lambdabehave.example;
+
+import com.insightfullogic.lambdabehave.JunitBehaveRunner;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+import static com.insightfullogic.lambdabehave.Lets.describe;
+
+/**
+ * .
+ */
+@RunWith(JunitBehaveRunner.class)
+public class MockingSpec extends Mockito {
+
+    interface Subscriber extends Consumer<String> {}
+
+    class Publisher {
+        private final List<Subscriber> subscribers;
+
+        Publisher() {
+            subscribers = new ArrayList<>();
+        }
+
+        public void addSubscriber(Subscriber subscriber) {
+            subscribers.add(subscriber);
+        }
+
+        public void send(String message) {
+            subscribers.forEach(subscriber -> subscriber.accept(message));
+        }
+    }
+
+    {
+
+        Publisher publisher = new Publisher();
+        Subscriber subscriber = mock(Subscriber.class);
+        Subscriber subscriber2 = mock(Subscriber.class);
+        Subscriber subscriber3 = mock(Subscriber.class);
+        publisher.addSubscriber(subscriber);
+        publisher.addSubscriber(subscriber2);
+
+    describe("should send messages to all subscribers", it -> {
+        when:
+        publisher.send("hello world");
+
+        then:
+        subscriber.accept("hello world");
+        subscriber2.accept("hello world");
+    });
+
+}}
