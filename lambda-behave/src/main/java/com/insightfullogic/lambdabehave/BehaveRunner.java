1d89fc7c58b6d330f2e689d95cd46a3046a2eba5
Richard Warburton <richard.warburton@gmail.com>
2014-06-07 23:49:00 +0100

    remove the reflections stuff in order to simplify dependencies and configuration

--- lambda-behave/src/main/java/com/insightfullogic/lambdabehave/BehaveRunner.java
+++ lambda-behave/src/main/java/com/insightfullogic/lambdabehave/BehaveRunner.java
@@ -4,16 +4,9 @@ import com.insightfullogic.lambdabehave.impl.output.ConsoleFormatter;
 import com.insightfullogic.lambdabehave.impl.output.ReportFormatter;
 import com.insightfullogic.lambdabehave.impl.reports.Report;
 import com.insightfullogic.lambdabehave.impl.reports.ReportStore;
-import org.reflections.Reflections;
-import org.reflections.scanners.ResourcesScanner;
-import org.reflections.scanners.SubTypesScanner;
-import org.reflections.util.ClasspathHelper;
-import org.reflections.util.ConfigurationBuilder;
-import org.reflections.util.FilterBuilder;
 
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -48,35 +41,8 @@ public final class BehaveRunner {
         try {
             return Stream.of(Class.forName(name));
         } catch (ClassNotFoundException e) {
-            return tryToLoadPackage(name);
+            throw new IllegalArgumentException(e);
         }
-    }
-
-    private static Stream<Class<?>> tryToLoadPackage(String name) {
-        int splitPoint = name.indexOf('*');
-        if (splitPoint == -1) {
-            throw new IllegalArgumentException("Invalid specification specifier: " + name);
-        }
-
-        String packageName = name.substring(0, splitPoint);
-        String classSuffix = name.substring(splitPoint + 1, name.length());
-
-        Reflections reflections = makeReflections(packageName);
-        return reflections.getSubTypesOf(Object.class)
-                          .stream()
-                          .filter(cls -> cls.getSimpleName().endsWith(classSuffix));
-    }
-
-    private static Reflections makeReflections(String packageName) {
-        // This BS is required to get subclasses of Object
-        List<ClassLoader> classLoadersList = new LinkedList<ClassLoader>();
-        classLoadersList.add(ClasspathHelper.contextClassLoader());
-        classLoadersList.add(ClasspathHelper.staticClassLoader());
-
-        return new Reflections(new ConfigurationBuilder()
-                    .setScanners(new SubTypesScanner(false /* don't exclude Object.class */), new ResourcesScanner())
-                    .setUrls(ClasspathHelper.forClassLoader(classLoadersList.toArray(new ClassLoader[0])))
-                    .filterInputsBy(new FilterBuilder().include(FilterBuilder.prefix(packageName))));
     }
 
     private final List<Class<?>> specifications;
