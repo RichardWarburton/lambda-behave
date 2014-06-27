835de5c1b206a91358ccb6149bb72f977a5b4d98
Richard Warburton <richard.warburton@gmail.com>
2014-06-27 21:31:52 +0100

    add example multiple value generated spec

--- src/test/java/com/insightfullogic/lambdabehave/example/GeneratedSpec.java
+++ src/test/java/com/insightfullogic/lambdabehave/example/GeneratedSpec.java
@@ -5,17 +5,26 @@ import org.junit.runner.RunWith;
 
 import static com.insightfullogic.lambdabehave.Suite.describe;
 import static com.insightfullogic.lambdabehave.generators.Example.asciiStrings;
+import static com.insightfullogic.lambdabehave.generators.Example.integersUpTo;
+import static com.insightfullogic.lambdabehave.generators.Example.strings;
 
 @RunWith(JunitSuiteRunner.class)
 public class GeneratedSpec {{
 
     describe("a String", it -> {
 
-        it.requires(100)
+        it.requires(10)
           .example(asciiStrings())
           .toShow("reversing a String twice returns the original String", (expect, str) -> {
               String same = new StringBuilder(str).reverse().reverse().toString();
               expect.that(same).isEqualTo(str);
+          });
+
+        it.requires(10)
+          .example(strings(), integersUpTo(9))
+          .toShow("adding a digit to the end of the String increments its length by one", (expect, str, x) -> {
+              String result = str + x;
+              expect.that(result.length()).is(str.length() + 1);
           });
 
     });
--- src/test/java/com/insightfullogic/lambdabehave/example/random_trees/Runner.java
+++ src/test/java/com/insightfullogic/lambdabehave/example/random_trees/Runner.java
@@ -13,4 +13,5 @@ public class Runner {
 
         tree.print(0);
     }
+
 }
