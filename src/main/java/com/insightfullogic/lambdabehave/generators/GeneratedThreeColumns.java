0bf198a6e858b3cb20854098088d5931d53a3b98
Richard Warburton <richard.warburton@gmail.com>
2014-07-02 22:48:57 +0100

    javadoc

--- src/main/java/com/insightfullogic/lambdabehave/generators/GeneratedThreeColumns.java
+++ src/main/java/com/insightfullogic/lambdabehave/generators/GeneratedThreeColumns.java
@@ -2,8 +2,17 @@ package com.insightfullogic.lambdabehave.generators;
 
 import com.insightfullogic.lambdabehave.specifications.ThreeColumnDataSpecification;
 
+/**
+ * Represents a three column table of data that has been automatically generated.
+ */
 public interface GeneratedThreeColumns<F, S, T> {
 
+    /**
+     * Specify the actual behaviour.
+     *
+     * @param description a human readable description of the behaviour you're expecting.
+     * @param specification a function which describes in code the expected behaviour.
+     */
     void toShow(String description, ThreeColumnDataSpecification<F, S, T> specification);
 
 }
